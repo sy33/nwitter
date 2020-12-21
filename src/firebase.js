15d256947af77fd8bc57783bf30ae6e0ae168b78
sy33 <user@email.mail>
2020-12-22 03:06:17 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDq1BaHF7yUJ5RvLFT3p47nUL53iHqlJZc",
+    authDomain: "nwitter-cee5c.firebaseapp.com",
+    projectId: "nwitter-cee5c",
+    storageBucket: "nwitter-cee5c.appspot.com",
+    messagingSenderId: "63921390440",
+    appId: "1:63921390440:web:1395221521d2b1318d8cdd"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
